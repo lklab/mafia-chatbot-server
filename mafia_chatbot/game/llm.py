3655e99bccb1df3f3ce75703a45e4fc629927fd0
lklab <alexeibatic@gmail.com>
2024-09-24 22:44:58 +0900

    strategy 리팩토링에 따른 프롬프트 수정

--- mafia_chatbot/game/llm.py
+++ mafia_chatbot/game/llm.py
@@ -68,7 +68,7 @@ class LLM :
                 'surviving_mafia_count': len(gameState.mafiaPlayers),
                 'survivors_list': ','.join(map(lambda p: p.info.name, gameState.players)),
                 'discussion_history': '\n'.join(gameState.discussionHistory),
-                'discussion_target': player.strategy.mainTarget.name,
+                'discussion_assumptions': ', '.join(map(lambda asm: f'the role of {asm[0].name} is {asm[1].name.lower()}', player.strategy.assumptions)),
                 'discussion_reason': player.strategy.reason,
             }
 
@@ -105,7 +105,7 @@ class LLM :
             "Discussion History:\n{discussion_history}"
         ])
         strategyTemplateText = (
-            "You must suspect {discussion_target} of being the mafia. Refer to the following as evidence."
+            "You must argue that {discussion_assumptions}, based on the following evidence."
             "\n{discussion_reason}"
         )
 
