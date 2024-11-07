9b14b27c8e069bf4a49371443d88167319a59634
lklab <alexeibatic@gmail.com>
2024-11-07 14:18:13 +0900

    클라이언트 연결 상태 관리

--- mafia_chatbot/network/tcp_client_handler.py
+++ mafia_chatbot/network/tcp_client_handler.py
@@ -1,11 +1,19 @@
 import asyncio
 from collections import deque
 from typing import Callable, Deque
+from enum import Enum
+
+class TcpClientState(Enum) :
+    CONNECTED = 0
+    LISTENING = 1
+    DISCONNECTED = 2
 
 delimiter = b'\xCA\xFE\xBA\xBE'
 
 class TcpClientHandler :
     def __init__(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter) :
+        self.state = TcpClientState.CONNECTED
+
         self.reader = reader
         self.writer = writer
 
@@ -16,7 +24,12 @@ class TcpClientHandler :
         print(f"Client connected: {self.addr}")
 
     async def listen(self, onMessage: Callable[[int, bytes], None], onDisconnected: Callable[[], None]) :
+        if self.state != TcpClientState.CONNECTED :
+            return
+        self.state = TcpClientState.LISTENING
+
         buffer = b''
+        self.onDisconnected = onDisconnected
 
         try:
             while True:
@@ -58,14 +71,15 @@ class TcpClientHandler :
             print(f"Error: {e}")
 
         print("Closing connection")
-        self.writer.close()
-        await self.writer.wait_closed()
-        onDisconnected()
+        await self._close()
 
     def sendStr(self, type: int, payload: str) :
         self.send(type, payload.encode())
 
     def send(self, type: int, payload: bytes) :
+        if self.state == TcpClientState.DISCONNECTED :
+            return
+
         data = (
             delimiter +
             type.to_bytes(4, byteorder='big') +
@@ -78,9 +92,24 @@ class TcpClientHandler :
             self.isSending = True
             asyncio.create_task(self._send())
 
+    async def _close(self) :
+        if self.state == TcpClientState.DISCONNECTED :
+            return
+
+        self.state = TcpClientState.DISCONNECTED
+        self.writer.close()
+        await self.writer.wait_closed()
+        self.onDisconnected()
+
     async def _send(self) :
-        while len(self.sendQueue) > 0 :
-            data = self.sendQueue.popleft()
-            self.writer.write(data)
-            await self.writer.drain()
+        try:
+            while len(self.sendQueue) > 0 and self.state == TcpClientState.LISTENING :
+                data = self.sendQueue.popleft()
+                self.writer.write(data)
+                await self.writer.drain()
+
+        except (OSError, asyncio.CancelledError) as e:
+            print(f"fail to send data: {e}")
+            await self._close()
+
         self.isSending = False
