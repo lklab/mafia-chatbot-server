3b2a0ff2b5e586496ba03aa71f4da4a3fdb24b30
lklab <alexeibatic@gmail.com>
2024-12-27 20:00:13 +0900

    pot 파일 업데이트 되면 기존 po 파일을 업데이트하도록 수정

--- mafia_chatbot/locales/tools/xgettext.py
+++ mafia_chatbot/locales/tools/xgettext.py
@@ -1,19 +1,36 @@
+import os
 import subprocess
 
 srcFiles: list[str] = [
     'mafia_chatbot/game/game_manager.py',
     'mafia_chatbot/game/game_state.py',
 ]
-destFile: str = 'mafia_chatbot/locales/messages.pot'
+potFile: str = 'mafia_chatbot/locales/messages.pot'
+locales: list[str] = [
+    'en',
+    'ko',
+]
 
 result = subprocess.run([
     'xgettext',
     '-d',
     'messages',
     '-o',
-    destFile,
+    potFile,
 ] + srcFiles, capture_output=True, text=True)
 
 if result.returncode != 0 :
     print(f'\033[31mfail to xgettext:\033[0m {result.stderr}')
     exit()
+
+for locale in locales :
+    result = subprocess.run([
+        'msgmerge',
+        '-U',
+        os.path.join('mafia_chatbot/locales', locale, 'LC_MESSAGES/messages.po'),
+        potFile,
+    ])
+
+    if result.returncode != 0 :
+        print(f'\033[31mfail to xgettext:\033[0m {result.stderr}')
+        exit()
